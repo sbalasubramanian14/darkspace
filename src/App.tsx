02c98de65acdd465d44e6628c5a24c9f11f022b5
Balasubramanian Sankareswaran <Balasubramanian.Sankareswaran@quantium.com.au>
2019-08-20 17:09:18 +0530

    Adding material theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import Layout from "./containers/Layout";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>App Ready</p>
-      </header>
+      </header> */}
+      <Layout />
     </div>
   );
 };
